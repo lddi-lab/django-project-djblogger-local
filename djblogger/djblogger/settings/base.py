20b6f21bf3c563769374d591de4bb86c92a6beb2
Lucky Icban <73286899+lddi-lab@users.noreply.github.com>
2023-12-28 14:35:26 +0800

    Build: Create and Register the Blog App

--- djblogger/djblogger/settings/base.py
+++ djblogger/djblogger/settings/base.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-]
+    "djblogger.blog",
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
