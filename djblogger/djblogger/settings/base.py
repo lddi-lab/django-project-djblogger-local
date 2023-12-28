81632bbeeb8d7292b269781d540bb6b7884defc0
Lucky Icban <73286899+lddi-lab@users.noreply.github.com>
2023-12-28 14:55:11 +0800

    Build: Model Migrations

--- djblogger/djblogger/settings/base.py
+++ djblogger/djblogger/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "djblogger.blog",
+]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
