7ae04e80061cb7f94334ab3c3dbc20c9c1a979b7
Lucky Icban <73286899+lddi-lab@users.noreply.github.com>
2024-01-08 18:34:50 +0800

    Testing: Homepage

--- djblogger/djblogger/tests/blog/test_models.py
+++ djblogger/djblogger/tests/blog/test_models.py
@@ -5,5 +5,9 @@ pytestmark = pytest.mark.django_db
 
 class TestPostModel:
     def test_str_return(self, post_factory):
-        post = post_factory(title="post-test")
-        assert post.__str__() == "post-test"
+        post = post_factory(title="test-post")
+        assert post.__str__() == "test-post"
+
+    def test_add_tag(self, post_factory):
+        x = post_factory(title="test-post", tags=["test-tag"])
+        assert x.tags.count() == 1
