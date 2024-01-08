80bc6d96c8519f3468e435b8fbfe0ae87108507b
Lucky Icban <73286899+lddi-lab@users.noreply.github.com>
2024-01-08 18:01:19 +0800

    Build: Tag Cloud

--- djblogger/djblogger/settings/base.py
+++ djblogger/djblogger/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "djblogger.blog",
     "django_htmx",
+    "taggit",
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "builtins": ["djblogger.blog.templatetags.tag_cloud"],
         },
     },
 ]
